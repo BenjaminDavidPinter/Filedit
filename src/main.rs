bd892ebb1b709b31238da74c58f4543694a2cc2d
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-03-25 11:12:17 -0400

    Validate PNG Signature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{fs, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = fs::File::open("/Users/benjaminpinter/Desktop/SS.png").unwrap();
+    let mut file_bytes = vec![];
+    let file_size = file.read_to_end(&mut file_bytes).unwrap();
+    println!("Read {:?} bytes", file_size);
+    println!(
+        "Valid PNG Signature: {:?}",
+        //First 8 bytes of a png file are the same.
+        check_png_signature(&file_bytes[0..8])
+    );
+}
+
+fn check_png_signature(bytes: &[u8]) -> bool {
+    let signature: Vec<u8> = vec![137, 80, 78, 71, 13, 10, 26, 10];
+    return signature.eq(bytes);
 }
