4a5652cfcc74eb11c6c1c736e11e8e289b263e7b
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-04-02 23:44:43 -0400

    Begin interpreting ICC Profile Data, using ICC.1:2010

--- src/png/chunks/iCCP.rs
+++ src/png/chunks/iCCP.rs
@@ -31,6 +31,42 @@ impl iCCP {
         let total_length = self.get_profile_name().len() + 2;
         decode_reader(self.data[total_length..].to_vec()).unwrap()
     }
+
+    pub fn get_profile_size(iccp_profile_bytes: &Vec<u8>) -> u32 {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[0..4]);
+        u32::from_be_bytes(buf)
+    }
+
+    pub fn get_profile_preferred_CMM_type(iccp_profile_bytes: &Vec<u8>) -> [u8; 4] {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[4..8]);
+        buf
+    }
+
+    pub fn get_profile_version(iccp_profile_bytes: &Vec<u8>) -> [u8; 4] {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[8..12]);
+        buf
+    }
+
+    pub fn get_profile_class(iccp_profile_bytes: &Vec<u8>) -> [u8; 4] {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[12..16]);
+        buf
+    }
+
+    pub fn get_color_space(iccp_profile_bytes: &Vec<u8>) -> [u8; 4] {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[16..20]);
+        buf
+    }
+
+    pub fn get_pcs_encoding(iccp_profile_bytes: &Vec<u8>) -> [u8; 4] {
+        let mut buf: [u8; 4] = [0; 4];
+        buf.copy_from_slice(&iccp_profile_bytes[20..24]);
+        buf
+    }
 }
 
 pub fn from_base_chunk(base_chunk: &BaseChunk) -> iCCP {
@@ -52,7 +88,34 @@ pub fn print_chunk(iccp_chunk: &iCCP) {
         "Compression Method: {:?}",
         iccp_chunk.get_compression_method()
     );
-    println!("Profile Data: {:?}", iccp_chunk.get_profile());
+    println!("Profile Data:");
+    println!(
+        "\tProfile Size: {:?}",
+        iCCP::get_profile_size(&iccp_chunk.get_profile())
+    );
+    println!(
+        "\tPreferred CMM: {:?}",
+        String::from_utf8_lossy(&iCCP::get_profile_preferred_CMM_type(
+            &iccp_chunk.get_profile()
+        ))
+    );
+    let version = iCCP::get_profile_version(&iccp_chunk.get_profile());
+    println!(
+        "\tProfile Version: {:?}.{:?}.{:?}.{:?}",
+        version[0], version[1], version[2], version[3]
+    );
+    println!(
+        "\tProfile Class: {:?}",
+        String::from_utf8_lossy(&iCCP::get_profile_class(&iccp_chunk.get_profile()))
+    );
+    println!(
+        "\tColor Space: {:?}",
+        String::from_utf8_lossy(&iCCP::get_color_space(&iccp_chunk.get_profile()))
+    );
+    println!(
+        "\tPCS Encoding: {:?}",
+        String::from_utf8_lossy(&iCCP::get_pcs_encoding(&iccp_chunk.get_profile()))
+    );
 }
 
 #[derive(Debug)]
